11eb8176107af1bc25f67b68cbdd7c1c3ca6b83d
Dmitriy <dmyanson@gmail.com>
2024-03-19 19:57:24 +0100

    add generator password in script.js and index.html

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Hello World!');
+
+function generatePassword() {
+    // Допустимые символы для генерации пароля
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]|;:,.<>?";
+
+    // Длина пароля
+    const length = 12;
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      // Выбираем случайный символ из chars и добавляем его к паролю
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      password += chars[randomIndex];
+    }
+
+    // Отображаем сгенерированный пароль на странице
+    document.getElementById("password").textContent = password;
+  }
